b638cb953437b6dbcef7173b53096241405e9cff
unknown <adhiragathan12@gmail.com>
2020-11-07 09:04:54 +0700

    initial commit

---
+++ src/if1/pkg10119025/latihan40/warnakepribadian/Latihan40WarnaKepribadian.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119025.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author User
+ * * NAMA     : ADHIRA FAHRI GATHAN
+ * KELAS    : PBO
+ * NIM      : 10119025
+ * Deskripsi Program : Program ini berisi tentang Warna Kepribadian
+ */
+public class Latihan40WarnaKepribadian {
+    public static void main(String[] args) {
+        User user = new User();
+        user.masukkanWarna();
+        user.masukkanNama();
+        user.menentukanKepribadian();
+        System.out.println("Developed by : Adhira Fahri Gathan");
+    }
+}
